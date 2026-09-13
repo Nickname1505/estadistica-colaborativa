058b01a1e4a9db0c895996e906b8491a2e28d6e1
raul <haloce2005@gmail.com>
2026-09-13 16:32:13 -0600

    Agregar desviacion estandar

--- estadistica.py
+++ estadistica.py
@@ -9,6 +9,12 @@ datos = np.loadtxt(
 ingresos = datos[:, 1]
 
 # MEDIDAS DE TENDENCIA CENTRAL
+media = np.mean(ingresos)
+print("Media del ingreso:", media)
 
+mediana = np.median(ingresos)
+print("Mediana del ingreso:", mediana)
 
 # MEDIDAS DE DISPERSION
+desviacion = np.std(ingresos)
+print("Desviacion estandar del ingreso:", desviacion)
